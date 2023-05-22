45a96d6ddbafd1aaf3e795b53298a598980a8c4f
praworks <prashan2011th@gmail.com>
2023-05-22 15:53:04 +0530

    Final

---
+++ ITE_1112_VS/Week 4/Activity 4/Beverages_1/Condition.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Beverages_1
+{
+    partial class Condition
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnIf = new System.Windows.Forms.Button();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter number 1 to 5";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(16, 30);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(215, 20);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.Text = "Enter Numbers Only";
+            this.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnIf
+            // 
+            this.btnIf.Location = new System.Drawing.Point(16, 78);
+            this.btnIf.Name = "btnIf";
+            this.btnIf.Size = new System.Drawing.Size(215, 23);
+            this.btnIf.TabIndex = 3;
+            this.btnIf.Text = "1. Choose Beverages using IF";
+            this.btnIf.UseVisualStyleBackColor = true;
+            this.btnIf.Click += new System.EventHandler(this.btnIf_Click);
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(16, 129);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(215, 23);
+            this.btnSwitch.TabIndex = 4;
+            this.btnSwitch.Text = "2. Choose Beverages using Switch";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Using IF Statement";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Using Switch Statement";
+            // 
+            // Condition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 207);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.btnIf);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Condition";
+            this.Text = "Condition";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnIf;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ITE_1112_VS/Week 4/Activity 4/Beverages_1/Condition.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Beverages_1
+{
+    public partial class Condition : Form
+    {
+        public Condition()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIf_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int isValidation;
+                if (int.TryParse(txtInput.Text, out isValidation))
+                {
+                    if (int.Parse(txtInput.Text) == 1)
+                    {
+                        MessageBox.Show("Number 1 is: Coke");
+                    }
+                    else if (int.Parse(txtInput.Text) == 2)
+                    {
+                        MessageBox.Show("Number 2 is: Wine");
+                    }
+                    else if (int.Parse(txtInput.Text) == 3)
+                    {
+                        MessageBox.Show("Number 3 is: Sprite");
+                    }
+                    else if (int.Parse(txtInput.Text) == 4)
+                    {
+                        MessageBox.Show("Number 4 is: Fanta");
+                    }
+                    else if (int.Parse(txtInput.Text) == 5)
+                    {
+                        MessageBox.Show("Number 5 is: Barley");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Out of range");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid number");
+                }
+                ClearText();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int isValidation;
+                if (int.TryParse(txtInput.Text, out isValidation))
+                {
+                    switch (int.Parse(txtInput.Text))
+                    {
+                        case 1:
+                            MessageBox.Show("Number 1 is: Coke");
+                            break;
+                        case 2:
+                            MessageBox.Show("Number 2 is: Wine");
+                            break;
+                        case 3:
+                            MessageBox.Show("Number 3 is: Sprite");
+                            break;
+                        case 4:
+                            MessageBox.Show("Number 4 is: Fanta");
+                            break;
+                        case 5:
+                            MessageBox.Show("Number 5 is: Barley");
+                            break;
+                        default:
+                            MessageBox.Show("Out of range");
+                            break;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid number");
+                }
+                ClearText();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ClearText()
+        {
+            txtInput.Text = string.Empty;
+            txtInput.Focus();
+        }
+
+        private void txtInput_Enter(object sender, EventArgs e)
+        { 
+            txtInput.Text = string.Empty; 
+        }
+    }
+}
