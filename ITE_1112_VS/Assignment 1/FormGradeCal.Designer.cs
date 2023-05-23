2a054edb661555a0ead65d9eb742d37a0aea7632
praworks <prashan2011th@gmail.com>
2023-05-23 09:58:09 +0530

    202305230958

---
+++ ITE_1112_VS/Assignment 1/FormGradeCal.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Assignment_1
+{
+    partial class FormGradeCal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMC = new System.Windows.Forms.Label();
+            this.txtMC = new System.Windows.Forms.TextBox();
+            this.txtMN = new System.Windows.Forms.TextBox();
+            this.labelMN = new System.Windows.Forms.Label();
+            this.txtCAW = new System.Windows.Forms.TextBox();
+            this.labelCAW = new System.Windows.Forms.Label();
+            this.txtSN = new System.Windows.Forms.TextBox();
+            this.labelSN = new System.Windows.Forms.Label();
+            this.txtRN = new System.Windows.Forms.TextBox();
+            this.labelRN = new System.Windows.Forms.Label();
+            this.txtFE = new System.Windows.Forms.TextBox();
+            this.labelFE = new System.Windows.Forms.Label();
+            this.txtCA = new System.Windows.Forms.TextBox();
+            this.labelCA = new System.Windows.Forms.Label();
+            this.txtSA = new System.Windows.Forms.TextBox();
+            this.labelSA = new System.Windows.Forms.Label();
+            this.label100 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelMC
+            // 
+            this.labelMC.AutoSize = true;
+            this.labelMC.Location = new System.Drawing.Point(12, 16);
+            this.labelMC.Name = "labelMC";
+            this.labelMC.Size = new System.Drawing.Size(69, 13);
+            this.labelMC.TabIndex = 0;
+            this.labelMC.Text = "Module code";
+            // 
+            // txtMC
+            // 
+            this.txtMC.Location = new System.Drawing.Point(205, 13);
+            this.txtMC.Name = "txtMC";
+            this.txtMC.Size = new System.Drawing.Size(258, 20);
+            this.txtMC.TabIndex = 1;
+            // 
+            // txtMN
+            // 
+            this.txtMN.Location = new System.Drawing.Point(205, 39);
+            this.txtMN.Name = "txtMN";
+            this.txtMN.Size = new System.Drawing.Size(258, 20);
+            this.txtMN.TabIndex = 3;
+            // 
+            // labelMN
+            // 
+            this.labelMN.AutoSize = true;
+            this.labelMN.Location = new System.Drawing.Point(12, 42);
+            this.labelMN.Name = "labelMN";
+            this.labelMN.Size = new System.Drawing.Size(71, 13);
+            this.labelMN.TabIndex = 2;
+            this.labelMN.Text = "Module name";
+            // 
+            // txtCAW
+            // 
+            this.txtCAW.Location = new System.Drawing.Point(205, 65);
+            this.txtCAW.Name = "txtCAW";
+            this.txtCAW.Size = new System.Drawing.Size(258, 20);
+            this.txtCAW.TabIndex = 5;
+            // 
+            // labelCAW
+            // 
+            this.labelCAW.AutoSize = true;
+            this.labelCAW.Location = new System.Drawing.Point(12, 68);
+            this.labelCAW.Name = "labelCAW";
+            this.labelCAW.Size = new System.Drawing.Size(170, 13);
+            this.labelCAW.TabIndex = 4;
+            this.labelCAW.Text = "Continuous assessment weightage";
+            // 
+            // txtSN
+            // 
+            this.txtSN.Location = new System.Drawing.Point(205, 91);
+            this.txtSN.Name = "txtSN";
+            this.txtSN.Size = new System.Drawing.Size(258, 20);
+            this.txtSN.TabIndex = 7;
+            // 
+            // labelSN
+            // 
+            this.labelSN.AutoSize = true;
+            this.labelSN.Location = new System.Drawing.Point(12, 94);
+            this.labelSN.Name = "labelSN";
+            this.labelSN.Size = new System.Drawing.Size(73, 13);
+            this.labelSN.TabIndex = 6;
+            this.labelSN.Text = "Student name";
+            // 
+            // txtRN
+            // 
+            this.txtRN.Location = new System.Drawing.Point(205, 117);
+            this.txtRN.Name = "txtRN";
+            this.txtRN.Size = new System.Drawing.Size(258, 20);
+            this.txtRN.TabIndex = 9;
+            // 
+            // labelRN
+            // 
+            this.labelRN.AutoSize = true;
+            this.labelRN.Location = new System.Drawing.Point(12, 120);
+            this.labelRN.Name = "labelRN";
+            this.labelRN.Size = new System.Drawing.Size(101, 13);
+            this.labelRN.TabIndex = 8;
+            this.labelRN.Text = "Registration number";
+            // 
+            // txtFE
+            // 
+            this.txtFE.Location = new System.Drawing.Point(205, 143);
+            this.txtFE.Name = "txtFE";
+            this.txtFE.Size = new System.Drawing.Size(258, 20);
+            this.txtFE.TabIndex = 11;
+            // 
+            // labelFE
+            // 
+            this.labelFE.AutoSize = true;
+            this.labelFE.Location = new System.Drawing.Point(12, 146);
+            this.labelFE.Name = "labelFE";
+            this.labelFE.Size = new System.Drawing.Size(183, 13);
+            this.labelFE.TabIndex = 10;
+            this.labelFE.Text = "End-semester examination (FE) marks";
+            // 
+            // txtCA
+            // 
+            this.txtCA.Location = new System.Drawing.Point(205, 169);
+            this.txtCA.Name = "txtCA";
+            this.txtCA.Size = new System.Drawing.Size(258, 20);
+            this.txtCA.TabIndex = 13;
+            // 
+            // labelCA
+            // 
+            this.labelCA.AutoSize = true;
+            this.labelCA.Location = new System.Drawing.Point(12, 172);
+            this.labelCA.Name = "labelCA";
+            this.labelCA.Size = new System.Drawing.Size(172, 13);
+            this.labelCA.TabIndex = 12;
+            this.labelCA.Text = "Continuous assessment (CA) marks";
+            // 
+            // txtSA
+            // 
+            this.txtSA.Location = new System.Drawing.Point(205, 195);
+            this.txtSA.Name = "txtSA";
+            this.txtSA.Size = new System.Drawing.Size(258, 20);
+            this.txtSA.TabIndex = 15;
+            // 
+            // labelSA
+            // 
+            this.labelSA.AutoSize = true;
+            this.labelSA.Location = new System.Drawing.Point(12, 198);
+            this.labelSA.Name = "labelSA";
+            this.labelSA.Size = new System.Drawing.Size(112, 13);
+            this.labelSA.TabIndex = 14;
+            this.labelSA.Text = " Student’s attendance";
+            // 
+            // label100
+            // 
+            this.label100.AutoSize = true;
+            this.label100.Location = new System.Drawing.Point(469, 68);
+            this.label100.Name = "label100";
+            this.label100.Size = new System.Drawing.Size(30, 13);
+            this.label100.TabIndex = 16;
+            this.label100.Text = "/100";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(469, 146);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(30, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "/100";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(469, 172);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(30, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "/100";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(469, 198);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(30, 13);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "/100";
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(205, 222);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(258, 23);
+            this.btnSub.TabIndex = 20;
+            this.btnSub.Text = "Calculate your grade";
+            this.btnSub.UseVisualStyleBackColor = true;
+            // 
+            // FormGradeCal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 259);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label100);
+            this.Controls.Add(this.txtSA);
+            this.Controls.Add(this.labelSA);
+            this.Controls.Add(this.txtCA);
+            this.Controls.Add(this.labelCA);
+            this.Controls.Add(this.txtFE);
+            this.Controls.Add(this.labelFE);
+            this.Controls.Add(this.txtRN);
+            this.Controls.Add(this.labelRN);
+            this.Controls.Add(this.txtSN);
+            this.Controls.Add(this.labelSN);
+            this.Controls.Add(this.txtCAW);
+            this.Controls.Add(this.labelCAW);
+            this.Controls.Add(this.txtMN);
+            this.Controls.Add(this.labelMN);
+            this.Controls.Add(this.txtMC);
+            this.Controls.Add(this.labelMC);
+            this.Name = "FormGradeCal";
+            this.Text = "Grade Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMC;
+        private System.Windows.Forms.TextBox txtMC;
+        private System.Windows.Forms.TextBox txtMN;
+        private System.Windows.Forms.Label labelMN;
+        private System.Windows.Forms.TextBox txtCAW;
+        private System.Windows.Forms.Label labelCAW;
+        private System.Windows.Forms.TextBox txtSN;
+        private System.Windows.Forms.Label labelSN;
+        private System.Windows.Forms.TextBox txtRN;
+        private System.Windows.Forms.Label labelRN;
+        private System.Windows.Forms.TextBox txtFE;
+        private System.Windows.Forms.Label labelFE;
+        private System.Windows.Forms.TextBox txtCA;
+        private System.Windows.Forms.Label labelCA;
+        private System.Windows.Forms.TextBox txtSA;
+        private System.Windows.Forms.Label labelSA;
+        private System.Windows.Forms.Label label100;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnSub;
+    }
+}
+
