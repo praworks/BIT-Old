62a2c227196e74f0ccab118e674642c5bd2c582a
praworks <prashan2011th@gmail.com>
2023-06-17 21:45:25 -0700

    kk

---
+++ ITE_1112_VS/Exam Prep/SGPA_Calculator/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace SGPA_Calculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.studentNameTextBox = new System.Windows.Forms.TextBox();
+            this.registrationNumberTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.module1CreditsTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.module2CreditsTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.module3CreditsTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.module4CreditsTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.module5CreditsTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.sgpaLabel = new System.Windows.Forms.Label();
+            this.module5GradeTextBox = new System.Windows.Forms.TextBox();
+            this.module4GradeTextBox = new System.Windows.Forms.TextBox();
+            this.module3GradeTextBox = new System.Windows.Forms.TextBox();
+            this.module2GradeTextBox = new System.Windows.Forms.TextBox();
+            this.module1GradeTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student name";
+            // 
+            // studentNameTextBox
+            // 
+            this.studentNameTextBox.Location = new System.Drawing.Point(251, 18);
+            this.studentNameTextBox.Name = "studentNameTextBox";
+            this.studentNameTextBox.Size = new System.Drawing.Size(257, 26);
+            this.studentNameTextBox.TabIndex = 1;
+            // 
+            // registrationNumberTextBox
+            // 
+            this.registrationNumberTextBox.Location = new System.Drawing.Point(251, 50);
+            this.registrationNumberTextBox.Name = "registrationNumberTextBox";
+            this.registrationNumberTextBox.Size = new System.Drawing.Size(257, 26);
+            this.registrationNumberTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Registration number";
+            // 
+            // module1CreditsTextBox
+            // 
+            this.module1CreditsTextBox.Location = new System.Drawing.Point(251, 82);
+            this.module1CreditsTextBox.Name = "module1CreditsTextBox";
+            this.module1CreditsTextBox.Size = new System.Drawing.Size(257, 26);
+            this.module1CreditsTextBox.TabIndex = 5;
+            this.module1CreditsTextBox.Text = "Credits";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Module 1";
+            // 
+            // module2CreditsTextBox
+            // 
+            this.module2CreditsTextBox.Location = new System.Drawing.Point(251, 114);
+            this.module2CreditsTextBox.Name = "module2CreditsTextBox";
+            this.module2CreditsTextBox.Size = new System.Drawing.Size(257, 26);
+            this.module2CreditsTextBox.TabIndex = 7;
+            this.module2CreditsTextBox.Text = "Credits";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Module 2";
+            // 
+            // module3CreditsTextBox
+            // 
+            this.module3CreditsTextBox.Location = new System.Drawing.Point(251, 146);
+            this.module3CreditsTextBox.Name = "module3CreditsTextBox";
+            this.module3CreditsTextBox.Size = new System.Drawing.Size(257, 26);
+            this.module3CreditsTextBox.TabIndex = 9;
+            this.module3CreditsTextBox.Text = "Credits";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Module 3";
+            // 
+            // module4CreditsTextBox
+            // 
+            this.module4CreditsTextBox.Location = new System.Drawing.Point(251, 178);
+            this.module4CreditsTextBox.Name = "module4CreditsTextBox";
+            this.module4CreditsTextBox.Size = new System.Drawing.Size(257, 26);
+            this.module4CreditsTextBox.TabIndex = 11;
+            this.module4CreditsTextBox.Text = "Credits";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Module 4";
+            // 
+            // module5CreditsTextBox
+            // 
+            this.module5CreditsTextBox.Location = new System.Drawing.Point(251, 210);
+            this.module5CreditsTextBox.Name = "module5CreditsTextBox";
+            this.module5CreditsTextBox.Size = new System.Drawing.Size(257, 26);
+            this.module5CreditsTextBox.TabIndex = 13;
+            this.module5CreditsTextBox.Text = "Credits";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 213);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Module 5";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(251, 242);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(257, 45);
+            this.calculateButton.TabIndex = 14;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // sgpaLabel
+            // 
+            this.sgpaLabel.AutoSize = true;
+            this.sgpaLabel.Location = new System.Drawing.Point(18, 318);
+            this.sgpaLabel.Name = "sgpaLabel";
+            this.sgpaLabel.Size = new System.Drawing.Size(55, 20);
+            this.sgpaLabel.TabIndex = 15;
+            this.sgpaLabel.Text = "Result";
+            // 
+            // module5GradeTextBox
+            // 
+            this.module5GradeTextBox.Location = new System.Drawing.Point(526, 210);
+            this.module5GradeTextBox.Name = "module5GradeTextBox";
+            this.module5GradeTextBox.Size = new System.Drawing.Size(257, 26);
+            this.module5GradeTextBox.TabIndex = 20;
+            this.module5GradeTextBox.Text = "Grade";
+            // 
+            // module4GradeTextBox
+            // 
+            this.module4GradeTextBox.Location = new System.Drawing.Point(526, 178);
+            this.module4GradeTextBox.Name = "module4GradeTextBox";
+            this.module4GradeTextBox.Size = new System.Drawing.Size(257, 26);
+            this.module4GradeTextBox.TabIndex = 19;
+            this.module4GradeTextBox.Text = "Grade";
+            // 
+            // module3GradeTextBox
+            // 
+            this.module3GradeTextBox.Location = new System.Drawing.Point(526, 146);
+            this.module3GradeTextBox.Name = "module3GradeTextBox";
+            this.module3GradeTextBox.Size = new System.Drawing.Size(257, 26);
+            this.module3GradeTextBox.TabIndex = 18;
+            this.module3GradeTextBox.Text = "Grade";
+            // 
+            // module2GradeTextBox
+            // 
+            this.module2GradeTextBox.Location = new System.Drawing.Point(526, 114);
+            this.module2GradeTextBox.Name = "module2GradeTextBox";
+            this.module2GradeTextBox.Size = new System.Drawing.Size(257, 26);
+            this.module2GradeTextBox.TabIndex = 17;
+            this.module2GradeTextBox.Text = "Grade";
+            // 
+            // module1GradeTextBox
+            // 
+            this.module1GradeTextBox.AcceptsTab = true;
+            this.module1GradeTextBox.Location = new System.Drawing.Point(526, 82);
+            this.module1GradeTextBox.Name = "module1GradeTextBox";
+            this.module1GradeTextBox.Size = new System.Drawing.Size(257, 26);
+            this.module1GradeTextBox.TabIndex = 16;
+            this.module1GradeTextBox.Text = "Grade";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 607);
+            this.Controls.Add(this.module5GradeTextBox);
+            this.Controls.Add(this.module4GradeTextBox);
+            this.Controls.Add(this.module3GradeTextBox);
+            this.Controls.Add(this.module2GradeTextBox);
+            this.Controls.Add(this.module1GradeTextBox);
+            this.Controls.Add(this.sgpaLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.module5CreditsTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.module4CreditsTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.module3CreditsTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.module2CreditsTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.module1CreditsTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.registrationNumberTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.studentNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox studentNameTextBox;
+        private System.Windows.Forms.TextBox registrationNumberTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox module1CreditsTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox module2CreditsTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox module3CreditsTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox module4CreditsTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox module5CreditsTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label sgpaLabel;
+        private System.Windows.Forms.TextBox module5GradeTextBox;
+        private System.Windows.Forms.TextBox module4GradeTextBox;
+        private System.Windows.Forms.TextBox module3GradeTextBox;
+        private System.Windows.Forms.TextBox module2GradeTextBox;
+        private System.Windows.Forms.TextBox module1GradeTextBox;
+    }
+}
+
